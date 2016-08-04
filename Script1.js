85a239d210ff124f51d1d212c20976a6b76e2150
Artūras Černiauskas <s023368@ad.viko.lt>
2016-08-05 01:32:33 +0300

    Custom parser`is done
    XML nuskaito, ir padaro objektų array, kokių reikia, liko D3 render`is

--- Script1.js
+++ Script1.js
@@ -1,27 +1,29 @@
 var targetDest = "http://213.197.173.50:3153/Staging/FutureDemographicsDemo-XML/PopulationData.xml"
+
+d3.xml(targetDest, d3CustomXMLParse);
+
 var xmlArray = []
 
-function d3CustomXMLParse(url) {
-	d3.xml(url, function(error, rawXML) {
-		if (error) throw error;
+function d3CustomXMLParse(error, rawXML) {
+	if (error) throw error;
 
-		// Convert the XML document to an array of objects.
-		// Note that querySelectorAll returns a NodeList, not a proper Array,
-		// so we must use map.call to invoke array methods.
-		var d = [].map.call(rawXML.querySelectorAll("Row"), function(Row) {
-			
-			var cell = [];
-			
-				for (i=0; i<Row.childElementCount; i++){
-					
-					cell[i] = Row.children[i].childNodes[0].nodeValue
-					
-				};
-			
-			return cell
-		});
-		xmlArray = makeObjectKeys(d)
+	// Convert the XML document to an array of objects.
+	// Note that querySelectorAll returns a NodeList, not a proper Array,
+	// so we must use map.call to invoke array methods.
+	var d = [].map.call(rawXML.querySelectorAll("Row"), function(Row) {
+		
+		var cell = [];
+		
+			for (i=0; i<Row.childElementCount; i++){
+				
+				cell[i] = Row.children[i].childNodes[0].nodeValue
+				
+			};
+		
+		return cell
 	});
+	xmlArray = makeObjectKeys(d)
+	console.log(xmlArray)
 };
 
 
@@ -47,3 +49,5 @@ function makeObjectKeys(parsedXML){
 	return result
 	
 };
+
+console.log(xmlArray)
